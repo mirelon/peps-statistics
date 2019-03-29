a92ecd4a47375c374294eac489eb0f0d9736d722
Michal Kováč <kovac@essential-data.sk>
2019-03-29 15:45:59 +0100

    STI

--- app/models/subtest.rb
+++ app/models/subtest.rb
@@ -1,19 +1,27 @@
 class Subtest < ApplicationRecord
   has_many :performances
+  self.inheritance_column = :pismeno
 
   # @return Integer Ziskane body
   def vyhodnot(file)
-    if pismeno == 'I'
-      table = CSV.parse(file, headers: false, col_sep: "\t")
-      table.filter{|line| line.first.include? 'Item'}.sum do |line|
-        a = line[3]
-        b = line[8]
-        if a and b and ((a.last == 'i' and b.last == 'L') or (a.last == '_' and b.last == 'R'))
-          1
-        else
-          0
-        end
-      end
+    table = CSV.parse(file, headers: false, col_sep: "\t")
+    table.filter{|line| line.first.include? 'Item'}.sum{|line| vyhodnot_riadok(line)}
+  end
+
+  def vyhodnot_riadok(line)
+    raise NotImplementedError
+  end
+end
+
+class I < Subtest
+  # @return Integer Ziskane body
+  def vyhodnot_riadok(line)
+    a = line[3]
+    b = line[8]
+    if a and b and ((a.last == 'i' and b.last == 'L') or (a.last == '_' and b.last == 'R'))
+      1
+    else
+      0
     end
   end
 end
