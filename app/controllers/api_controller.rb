14c124a4c296b77f092d797a5d230f615477b507
Michal Kováč <kovac@essential-data.sk>
2019-09-05 11:49:58 +0200

    Submit localStorage from peps2

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,38 @@
+class ApiController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  after_action :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
+  end
+
+  def options_add_performance
+    puts 'options'
+    head(:ok) if request.request_method == 'OPTIONS'
+  end
+
+  def add_performance
+    params.each do |key, value|
+      next unless key.is_a? String and value.is_a? String
+      key_parts = key.split('_')
+      value_parts = value.split('_')
+      puts key_parts.inspect
+      puts value_parts.inspect
+      if key_parts.size == 5 and value_parts.size == 2
+        subtest = Subtest.where(pismeno: key_parts[0]).first!
+        rodne_cislo = key_parts[1]
+        meno = key_parts[2]
+        priezvisko = key_parts[3]
+        sex = key_parts[4]
+        body = value_parts[0].to_i
+        datum = Date.parse(value_parts[1])
+        client = Client.first_or_create!(meno: meno, priezvisko: priezvisko, sex: sex, rodne_cislo: rodne_cislo)
+        client.performances.where(subtest: subtest, datum: datum).first_or_create!(body: body)
+        puts "#{client.display_name} - #{subtest.pismeno} - #{body}"
+      end
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'api/performance' => 'api#add_performance'
+  match 'api/performance', to: 'api#options_add_performance', via: [:options]
 end
