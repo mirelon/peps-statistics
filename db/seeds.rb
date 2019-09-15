30c7ed8540d6036167297a41b0f928b3b66c22bd
Michal Kováč <kovac@essential-data.sk>
2019-09-15 23:06:05 +0200

    Do not seed from Dropbox to Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,69 +11,69 @@ i = Subtest.where(pismeno: 'I').first_or_create!(pismeno: 'I', popis: 'Affect ex
 p = Subtest.where(pismeno: 'P').first_or_create!(pismeno: 'P', popis: 'Contrastive Stress understanding', filename: 'P_Contrastive Stress understanding.txt', stlpec_spravne: 7, stlpec_client: 5)
 q = Subtest.where(pismeno: 'Q').first_or_create!(pismeno: 'Q', popis: 'Contrastive Stress expression', filename: 'Q_Contrastive stress expression.txt', stlpec_spravne: 2, stlpec_client: 6)
 
-dropbox = DropboxApi::Client.new('iWCZek45F1AAAAAAAABAdDkFN8rQUi2BItdN-6o86i1cbRmusBjwY19d1Kxpu7Uo')
-
-def get_file(dropbox, path)
-  file = ''
-  dropbox.download(path) do |chunk|
-    file << chunk.force_encoding('iso-8859-2').encode('utf-8')
-  end
-  file
-end
-
-client_folders_from_dropbox = dropbox.list_folder('/PEPS-C_2015_UKGen').entries.map(&:name).filter{|name| /^[\d_]*$/.match? name}
-client_folders_from_db = Client.all.map(&:folder).compact
-client_folders_to_process = if ENV['only_new'] == 'clients'
-                              client_folders_from_dropbox - client_folders_from_db
-                            else
-                              client_folders_from_dropbox
-                            end
-pp client_folders_from_dropbox
-pp client_folders_from_db
-puts "Processing #{client_folders_to_process.size} folders from dropbox: #{client_folders_to_process}"
-client_folders_to_process.each do |client_folder|
-  attrs = get_file(dropbox,"/PEPS-C_2015_UKGen/#{client_folder}/Client.cln")
-                   .split("\n")
-                   .filter{|line| line.include? '='}
-                   .map{|line| line.split('=', -1)}
-                   .to_h
-  meno = attrs['GivenNames'].strip
-  priezvisko = attrs['FamilyName'].strip
-  sex = case attrs['Sex'].strip
-        when '1'
-          'M'
-        when '2'
-          'F'
-        end
-  rodne_cislo = attrs['ID'].strip
-  client = Client.where(folder: client_folder).first_or_create!(meno: meno, priezvisko: priezvisko, sex: sex, folder: client_folder, rodne_cislo: rodne_cislo)
-  puts client.display_name
-
-  dropbox.list_folder("/PEPS-C_2015_UKGen/#{client_folder}/Results").entries.map(&:name).each do |test_set_folder|
-    puts "    #{test_set_folder}"
-    task_file_names = dropbox.list_folder("/PEPS-C_2015_UKGen/#{client_folder}/Results/#{test_set_folder}").entries.map(&:name).filter{|name| /^[A-Z]/.match? name}
-    Subtest.all.each do |subtest|
-      next if ENV['only_new'] == 'performances' and client.performances.where(subtest: subtest).count > 0
-      task_file_names.select{|f| f == subtest.filename}.each do |filename|
-        puts "        #{filename}"
-        file = get_file(dropbox, "/PEPS-C_2015_UKGen/#{client_folder}/Results/#{test_set_folder}/#{filename}")
-        body = subtest.vyhodnot(file)
-        datum = /Date=(\d\d\/\d\d\/\d\d\d\d)/.match(file).captures.first
-        client.performances.create(subtest: subtest, body: body, datum: Date.parse(datum))
-      end
-    end
-  end
-
-  (1..16).each do |i|
-    rc = ''
-    if i < 10
-      rc += '0'
-    end
-    rc += i.to_s
-    rc += '0101'
-    client = Client.create!(meno: "Test #{i}", priezvisko: "Test #{i}", sex: 'M', folder: nil, rodne_cislo: rc)
-    body = Random.rand(16-i..16)
-    client.performances.create(subtest: Subtest.first, body: body, datum: Date.current)
-  end
-
-end
+# dropbox = DropboxApi::Client.new('iWCZek45F1AAAAAAAABAdDkFN8rQUi2BItdN-6o86i1cbRmusBjwY19d1Kxpu7Uo')
+#
+# def get_file(dropbox, path)
+#   file = ''
+#   dropbox.download(path) do |chunk|
+#     file << chunk.force_encoding('iso-8859-2').encode('utf-8')
+#   end
+#   file
+# end
+#
+# client_folders_from_dropbox = dropbox.list_folder('/PEPS-C_2015_UKGen').entries.map(&:name).filter{|name| /^[\d_]*$/.match? name}
+# client_folders_from_db = Client.all.map(&:folder).compact
+# client_folders_to_process = if ENV['only_new'] == 'clients'
+#                               client_folders_from_dropbox - client_folders_from_db
+#                             else
+#                               client_folders_from_dropbox
+#                             end
+# pp client_folders_from_dropbox
+# pp client_folders_from_db
+# puts "Processing #{client_folders_to_process.size} folders from dropbox: #{client_folders_to_process}"
+# client_folders_to_process.each do |client_folder|
+#   attrs = get_file(dropbox,"/PEPS-C_2015_UKGen/#{client_folder}/Client.cln")
+#                    .split("\n")
+#                    .filter{|line| line.include? '='}
+#                    .map{|line| line.split('=', -1)}
+#                    .to_h
+#   meno = attrs['GivenNames'].strip
+#   priezvisko = attrs['FamilyName'].strip
+#   sex = case attrs['Sex'].strip
+#         when '1'
+#           'M'
+#         when '2'
+#           'F'
+#         end
+#   rodne_cislo = attrs['ID'].strip
+#   client = Client.where(folder: client_folder).first_or_create!(meno: meno, priezvisko: priezvisko, sex: sex, folder: client_folder, rodne_cislo: rodne_cislo)
+#   puts client.display_name
+#
+#   dropbox.list_folder("/PEPS-C_2015_UKGen/#{client_folder}/Results").entries.map(&:name).each do |test_set_folder|
+#     puts "    #{test_set_folder}"
+#     task_file_names = dropbox.list_folder("/PEPS-C_2015_UKGen/#{client_folder}/Results/#{test_set_folder}").entries.map(&:name).filter{|name| /^[A-Z]/.match? name}
+#     Subtest.all.each do |subtest|
+#       next if ENV['only_new'] == 'performances' and client.performances.where(subtest: subtest).count > 0
+#       task_file_names.select{|f| f == subtest.filename}.each do |filename|
+#         puts "        #{filename}"
+#         file = get_file(dropbox, "/PEPS-C_2015_UKGen/#{client_folder}/Results/#{test_set_folder}/#{filename}")
+#         body = subtest.vyhodnot(file)
+#         datum = /Date=(\d\d\/\d\d\/\d\d\d\d)/.match(file).captures.first
+#         client.performances.create(subtest: subtest, body: body, datum: Date.parse(datum))
+#       end
+#     end
+#   end
+#
+#   (1..16).each do |i|
+#     rc = ''
+#     if i < 10
+#       rc += '0'
+#     end
+#     rc += i.to_s
+#     rc += '0101'
+#     client = Client.create!(meno: "Test #{i}", priezvisko: "Test #{i}", sex: 'M', folder: nil, rodne_cislo: rc)
+#     body = Random.rand(16-i..16)
+#     client.performances.create(subtest: Subtest.first, body: body, datum: Date.current)
+#   end
+#
+# end
