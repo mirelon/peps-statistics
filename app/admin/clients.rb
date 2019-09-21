b9d002744f04c01934aed3bac7b5f08e4189619c
Michal Kováč <kovac@essential-data.sk>
2019-09-21 20:53:02 +0200

    Allow proper editing of clients

--- app/admin/clients.rb
+++ app/admin/clients.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Client do
-  permit_params :rodne_cislo, :folder, :meno, :priezvisko
+  permit_params :rodne_cislo, :folder, :meno, :priezvisko, :sex
+
+  form do |f|
+    f.inputs do
+      f.input :rodne_cislo
+      f.input :folder
+      f.input :meno
+      f.input :priezvisko
+      f.input :sex, as: :select, collection: [:muz, :zena]
+    end
+    f.actions
+  end
 end
